70e2d7a37dc88e5599130830b545c64be73e739a
Suhani <suhani@stackaisolutions.com>
2026-06-02 00:50:24 +0530

    feat: add recommendations endpoint

--- app/routers/recommendation.py
+++ app/routers/recommendation.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter
 from app.schemas.recommendation_schema import RecommendationRequest
+from app.services.recommendation_service import get_recommendations
 
 router = APIRouter()
 
 @router.post("/recommendations")
 async def recommend(data: RecommendationRequest):
-    return{
-        "message" : "API is working",
-        "input" : data
+    result = await get_recommendations(data.mood, data.language)
+    return {
+        "success": True,
+        "status_code": 200,
+        "data": result
     }
