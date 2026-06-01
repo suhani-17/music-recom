12ccbee79ecb8e975046cc97cd71473ed28f1909
Suhani <suhani@stackaisolutions.com>
2026-06-02 00:50:48 +0530

    fix: add CORS middleware and clean up imports

--- app/main.py
+++ app/main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  
 from app.routers import recommendation
+
+from app.db.database import engine 
+from app.models.history import History
+
 
 app = FastAPI()
 
-@app.get("/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Adjust this in production to restrict origins
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)   
 
+@app.get("/")
 def root():
     return {"message": "Backend is running!"}
+
+#temporary code to create tables on startup, in production use alembic for migrations
+@app.on_event("startup")
+def create_tables():
+    History.metadata.create_all(bind=engine)
 
 app.include_router(recommendation.router)
