90d1b7c8b954448574349982b2c607ce68f9bea1
Suhani <suhani@stackaisolutions.com>
2026-06-02 01:33:31 +0530

    feat: save recommendation history to database

--- app/db/database.py
+++ app/db/database.py
@@ -10,3 +10,10 @@ engine = create_engine(DATABASE_URL)
 
 sessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/routers/recommendation.py
+++ app/routers/recommendation.py
@@ -1,12 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.db.database import get_db
 from app.schemas.recommendation_schema import RecommendationRequest
 from app.services.recommendation_service import get_recommendations
 
 router = APIRouter()
 
 @router.post("/recommendations")
-async def recommend(data: RecommendationRequest):
+async def recommend(data: RecommendationRequest, db: Session = Depends(get_db)):
     result = await get_recommendations(data.mood, data.language)
+
+    for song in result:
+        record = History(
+            mood = data.mood,
+            language = data.language,
+            song_id = song['id'],
+            title = song['title'],
+            artist = song['artist'],
+        )
+        db.add(record)
+
+        db.commit()
     return {
         "success": True,
         "status_code": 200,
